8655f2cc4d0a2f5ef79de1b5802de8ac1cfaa3ac
M0rl0ck <seriy17@yandex.ru>
2024-01-26 15:29:55 +0300

    feat: implement Worker Threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+import { getPathToFile } from "../getPaths.js";
+
 const performCalculations = async () => {
-    // Write your code here
+  const pathToWorker = getPathToFile(import.meta.url, "worker.js");
+  const cpuInfo = cpus();
+  let sendValue = 10;
+
+  const workersPromise = cpuInfo.map(
+    () =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker(pathToWorker, {
+          workerData: sendValue++,
+        });
+        worker.on("message", (message) => resolve(message));
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+          if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+      })
+  );
+
+  const workers = Promise.allSettled(workersPromise);
+
+  const result = (await workers).map(({ status, value }) =>
+    status === "fulfilled"
+      ? { status: "resolved", data: value }
+      : { status: "error", data: null }
+  );
+
+  console.log(result);
 };
 
 await performCalculations();
