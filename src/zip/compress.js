a37d3319301f1affecbb058884c67bd6520399e6
M0rl0ck <seriy17@yandex.ru>
2025-04-26 19:47:03 +0300

    feat: implement zip operation

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+import { join } from "path";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToSource = join(import.meta.dirname, "files", "fileToCompress.txt");
+  const pathToDestination = join(import.meta.dirname, "files", "archive.gz");
+
+  const sourceStream = createReadStream(pathToSource);
+  const destinationStream = createWriteStream(pathToDestination);
+  const gzipStream = createGzip();
+
+  await pipeline(sourceStream, gzipStream, destinationStream);
 };
 
 await compress();
